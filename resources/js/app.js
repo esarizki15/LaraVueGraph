11f33efe7466e55188de6c62c7885c53efefba2b
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2019-12-31 17:53:04 +0700

    first commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,19 +4,39 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+import 'vuetify/dist/vuetify.min.css'	
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
+// import LayoutComponent from '.components/LayoutComponent'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+Vue.use(VueApollo)
+Vue.use(Vuetify)
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('layout-component', require('./components/LayoutComponent.vue'));
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'http://localhost:8000/graphql'
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    apolloProvider,
+    vuetify: new Vuetify(),
 });
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,128 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <v-container> 
+            <v-col md="10" offset-md="1">
+            <template>
+                  <v-card 
+                  shaped>
+                    <v-card-title>
+                      Data Users
+                      <v-spacer></v-spacer>
+
+                      <v-text-field
+                        v-model="search"
+                        append-icon="search"
+                        label="Search"
+                        single-line
+                        hide-details
+                      ></v-text-field>
+                    </v-card-title>
+                  <v-card-actions>
+                    <v-btn small color="primary" @click="handleClick">Test Mutation</v-btn>
+                  </v-card-actions>
+                    
+                    <v-data-table
+                      :headers="headers"
+                      :items="users"
+                      :search="search"
+                    ></v-data-table>
+                  </v-card>
+            </template> 
+        </v-col>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
-
-<script>
+<script>    
+    import gql from 'graphql-tag'
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        props: {
+          source: String,
+        },
+        data(){
+            return{
+
+                users: [],
+                posts: [],
+                search: '',
+                headers: [
+                  {
+                    text: 'Name',
+                    align: 'left',
+                    sortable: false,
+                    value: 'name',
+                  },
+                  { text: 'Email', value: 'email' },
+                ],
+                
+                drawer: null,
+
+            }
+        },
+
+        apollo: {
+            users: gql`
+            {
+                users{
+                    name
+                    email
+                }
+            }`,
+            posts: gql`
+            {
+                posts{
+                    title
+                    user{
+                        name
+                        email
+                    }
+                }
+            }`,
+        },
+        methods:{
+            handleClick(){
+                console.log('berhasil')
+            }
         }
     }
 </script>
